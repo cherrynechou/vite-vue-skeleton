3e574284f691e64aec8024b256b25f88b544333a
unknown <cherrynechou>
2024-07-01 22:26:38 +0800

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
+  <a-config-provider  :theme="{
+    token: {
+      borderRadius: 2
+    }
+  }">
     <!-- algorithm: theme.darkAlgorithm,夜间主题 -->
     <router-view />
+  </a-config-provider>
 </template>
 
 <script setup lang="ts">
